1f4ae3d882340c2b5123cb04ac98bbb4a1f1e788
n3dhir <84473151+n3dhir@users.noreply.github.com>
2022-08-16 10:04:42 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 let cells = document.querySelectorAll(".wrapper > .grid > div > span");
-let btn = document.querySelector("button");
+let bfsButton = document.querySelector(".bfs-button");
+let dfsButton = document.querySelector(".dfs-button");
+let ASatrButton = document.querySelector(".a-star-button");
+let shuffleButton = document.querySelector(".shuffle-button");
+let moves = [[-1, 0, 3, 6], [1, 2, 5, 8], [-3, 0, 1, 2], [3, 6, 7, 8]];
 
 cells.forEach((cell) => {
   cell.addEventListener("click", (e) => {
+    clr();
     let neighbors = getNeighbors(e.target.id);
     for (let i = 0; i < neighbors.length; i++) {
       let neighbor = document.querySelector(`#${neighbors[i]}`);
@@ -40,12 +45,27 @@ function swap(cell, neighbor) {
   [cell.innerHTML, neighbor.innerHTML] = [neighbor.innerHTML, cell.innerHTML];
 }
 
-btn.addEventListener("click", () => {
-  solve();
+bfsButton.addEventListener("click", () => {
+  clr();
+  bfs();
+});
+
+dfsButton.addEventListener("click", () => {
+  clr();
+  dfs();
+});
+
+shuffleButton.addEventListener("click", () => {
+  clr();
+  shuffle();
+})
+ASatrButton.addEventListener("click", () => {
+  clr();
+  AStar();
 })
 
-function solve() {
-  let cells = document.querySelectorAll(".wrapper > .grid > div > span");
+function bfs() {
+  let nb = 0;
   let startNode = [];
   for (let i = 0; i < cells.length; i++)
     startNode.push(cells[i].innerHTML);
@@ -54,43 +74,80 @@ function solve() {
   let q = [];
   q.push(startNode);
   while (q.length) {
+    nb++;
     let currentNode = q.shift();
     if (check(currentNode)) {
-      reconstruct(par, currentNode);
+      let path = [];
+      while (par[currentNode] != currentNode) {
+        path.push(currentNode);
+        currentNode = par[currentNode];
+      }
+      show(nb, path.length);
+      print(path.length, path, 500);
       break;
     }
     for (let i = 0; i < currentNode.length; i++)
       if (currentNode[i] === '') {
-        if (i && i != 3 && i != 6) {
-          let neighbor = [...currentNode];
-          [neighbor[i], neighbor[i - 1]] = [neighbor[i - 1], neighbor[i]];
-          if (!par[neighbor]) {
-            par[neighbor] = currentNode;
-            q.push(neighbor);
+        for (let j = 0; j < moves.length; j++) {
+          let flag = true;
+          for (let k = 1; k < moves[j].length; k++) {
+            if (moves[j][k] == i) {
+              flag = false;
+              break;
+            }
+          }
+          if (flag) {
+            let neighbor = [...currentNode];
+            [neighbor[i], neighbor[i + moves[j][0]]] = [neighbor[i + moves[j][0]], neighbor[i]];
+            if (!par[neighbor]) {
+              par[neighbor] = currentNode;
+              q.push(neighbor);
+            }
           }
         }
-        if (i != 2 && i != 5 && i != 8) {
-          let neighbor = [...currentNode];
-          [neighbor[i], neighbor[i + 1]] = [neighbor[i + 1], neighbor[i]];
-          if (!par[neighbor]) {
-            par[neighbor] = currentNode;
-            q.push(neighbor);
+      }
+  }
+}
+
+function dfs() {
+  let nb = 0;
+  let startNode = [];
+  for (let i = 0; i < cells.length; i++)
+    startNode.push(cells[i].innerHTML);
+  let par = new Map();
+  par[startNode] = startNode;
+  let st = [];
+  st.push(startNode);
+  while (st.length) {
+    nb++;
+    let currentNode = st.pop();
+    if (check(currentNode)) {
+      let path = [];
+      while (par[currentNode] != currentNode) {
+        path.push(currentNode);
+        currentNode = par[currentNode];
+      }
+      show(nb, path.length);
+      print(path.length, path, 0);
+      break;
+    }
+    for (let i = 0; i < currentNode.length; i++)
+      if (currentNode[i] === '') {
+        for (let j = 0; j < moves.length; j++) {
+          let flag = true;
+          for (let k = 1; k < moves[j].length; k++) {
+            if (moves[j][k] == i) {
+              flag = false;
+              break;
+            }
           }
-        }
-        if (i > 2) {
-          let neighbor = [...currentNode];
-          [neighbor[i], neighbor[i - 3]] = [neighbor[i - 3], neighbor[i]];
-          if (!par[neighbor]) {
-            par[neighbor] = currentNode;
-            q.push(neighbor);
-          }
-        }
-        if (i < 6) {
-          let neighbor = [...currentNode];
-          [neighbor[i], neighbor[i + 3]] = [neighbor[i + 3], neighbor[i]];
-          if (!par[neighbor]) {
-            par[neighbor] = currentNode;
-            q.push(neighbor);
+          if (flag) {
+            let neighbor = [...currentNode];
+            [neighbor[i], neighbor[i + moves[j][0]]] = [neighbor[i + moves[j][0]], neighbor[i]];
+            if (!par[neighbor]) {
+              par[neighbor] = currentNode;
+              st.push(neighbor);
+            }
           }
         }
       }
@@ -104,24 +161,121 @@ function check(grid) {
   return true
 }
 
-async function reconstruct(par, node) {
-  let path = [];
-  while (par[node] != node) {
-    path.push(node);
-    node = par[node];
-  }
-  print(path.length, path);
-}
-
-function print(n, path) {
-  if(!n) {
+function print(n, path, speed) {
+  if (!n) {
     return;
   }
   setTimeout(() => {
-      for (let j = 0; j < 3; j++)
-          for (let k = 0; k < 3; k++) {
-              cells[j * 3 + k].innerHTML = path[n-1][j * 3 + k];
+    for (let j = 0; j < 3; j++)
+      for (let k = 0; k < 3; k++) {
+        cells[j * 3 + k].innerHTML = path[n - 1][j * 3 + k];
+      }
+    print(n - 1, path, speed);
+  }, speed);
+}
+
+function shuffle() {
+  for (let i = 0; i < 100; i++) {
+    let empty;
+    for (let i = 0; i < cells.length; i++)
+      if (cells[i].innerHTML === '') {
+        empty = cells[i];
+        break;
+      }
+    let neighbors = getNeighbors(empty.id);
+    let chosen = neighbors[Math.floor(Math.random() * neighbors.length)];
+    swap(document.querySelector(`#${chosen}`), empty);
+  }
+}
+
+function show(nb, len) {
+  let p = document.createElement("p");
+  p.className = "pop-up";
+  p.innerHTML = `<img src="xmark-solid.svg">Path Length: <span>${len}</span><br>Number Of Visited Nodes: <span>${nb}</span>`;
+  document.querySelector(".wrapper").prepend(p);
+}
+
+document.addEventListener("click", (e) => {
+  if (e.target.tagName === "IMG") {
+    let parent = e.target.parentElement;
+    parent.remove();
+  }
+})
+
+document.addEventListener("click", (e) => {
+  if (e.target.className === "pop-up") {
+    e.target.remove();
+  }
+})
+
+function clr() {
+  let popUp = document.querySelector(".pop-up");
+  if (popUp)
+    popUp.remove();
+}
+
+function heuristic(node) {
+  let ans = 0;
+  for (let i = 0; i < 8; i++)
+    ans += node[i] != i + 1;
+  ans += node[8] != '';
+  return ans;
+}
+
+function AStar() {
+  let nb = 0;
+  let startNode = [];
+  for (let i = 0; i < cells.length; i++)
+    startNode.push(cells[i].innerHTML);
+  let par = new Map();
+  let dist = new Map();
+  let vis = new Map();
+  par[startNode] = startNode;
+  dist[startNode] = 0;
+  let pq = [];
+  pq.push([0, startNode]);
+  while (pq.length) {
+    nb++;
+    let current = pq.shift();
+    let currentNode = current[1];
+    vis[currentNode] = true;
+    if(dist[currentNode] + heuristic(currentNode) < current[0])
+      continue;
+    if (check(currentNode)) {
+      let path = [];
+      while (par[currentNode] != currentNode) {
+        path.push(currentNode);
+        currentNode = par[currentNode];
+      }
+      show(nb, path.length);
+      print(path.length, path, 500);
+      break;
+    }
+    for (let i = 0; i < currentNode.length; i++)
+      if (currentNode[i] === '') {
+        for (let j = 0; j < moves.length; j++) {
+          let flag = true;
+          for (let k = 1; k < moves[j].length; k++) {
+            if (moves[j][k] == i) {
+              flag = false;
+              break;
+            }
           }
-          print(n-1, path);
-  }, 500);
+          if (flag) {
+            let neighbor = [...currentNode];
+            [neighbor[i], neighbor[i + moves[j][0]]] = [neighbor[i + moves[j][0]], neighbor[i]];
+            if(vis[neighbor])
+              continue;
+            if (!par[neighbor] || dist[neighbor] > dist[currentNode] + 1) {
+              par[neighbor] = currentNode;
+              dist[neighbor] = dist[currentNode] + 1;
+              let cost = dist[neighbor] + heuristic(neighbor);
+              let i;
+              for (i = 0; i < pq.length && cost >= pq[i][0]; i++);
+              pq.splice(i, 0, [cost, neighbor]);
+            }
+          }
+        }
+      }
+  }
 }
